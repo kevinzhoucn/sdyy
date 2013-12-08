fdf85847dd3e95701d5e4a51c9973afb15f414e2
kevin.zhou <kevin@ubuntu.(none)>
2013-12-08 09:25:06 +0800

    front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Sdyy::Application.routes.draw do
+  get "front/index"
+
   resources :blogs
+
+  root :to => "front#index", :as => "front"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
